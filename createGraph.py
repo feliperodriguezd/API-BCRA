425d7fcd5991683583f8fb2f02eb760178c43a24
feliperodriguezd <72653242+feliperodriguezd@users.noreply.github.com>
2024-05-12 15:38:46 -0300

    Update createGraph.py
    Clean code + add an option to show all the graphs that can be generated

--- createGraph.py
+++ createGraph.py
@@ -3,13 +3,8 @@ import urllib3
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-urllib3.disable_warnings()
-response = requests.get("https://api.bcra.gob.ar/estadisticas/v1/principalesvariables", verify=False)
 
-responseInJson = response.json()
-resultsVariables = responseInJson["results"]
-
-def CreateGraph(idVariable, diaDesde, mesDesde, anioDesde, diaHasta, mesHasta, anioHasta):
+def CreateGraph(idVariable, diaDesde, mesDesde, anioDesde, diaHasta, mesHasta, anioHasta, onlyResultsVariables):
     buscar = requests.get("https://api.bcra.gob.ar/estadisticas/v1/datosvariable/{v}/{ad}-{md}-{dd}/{ah}-{mh}-{dh}".format(v = idVariable, ad = anioDesde, md = mesDesde, dd =  diaDesde, ah = anioHasta, mh = mesHasta, dh = diaHasta), verify=False)
 
     buscarInJson = buscar.json()
@@ -30,42 +25,74 @@ def CreateGraph(idVariable, diaDesde, mesDesde, anioDesde, diaHasta, mesHasta, a
     ax.xaxis.set_major_locator(plt.MaxNLocator(6))
     ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x)))
     ax.yaxis.grid(True)
-    for variable in resultsVariables:
+    for variable in onlyResultsVariables:
         if variable["idVariable"] == idVariable:
             plt.title(variable["descripcion"])
     plt.show()
 
 
-def ConfirmarGrafico(id):
-    for variable in resultsVariables:
+def ConfirmarGrafico(id, onlyResultsVariables):
+    for variable in onlyResultsVariables:
         if variable["idVariable"] == id:
             return variable["descripcion"]
     raise NameError()
 
-confirmacion = "N"
-try:
+
+def GetId(onlyResultsVariables):
+    confirmacion = "N"
     while (confirmacion != "Y"):
         print("Ingrese el ID deseado:")
         id = input()
-        graphName = ConfirmarGrafico(int(id))
+        graphName = ConfirmarGrafico(int(id), onlyResultsVariables)
         print(graphName)
         print("¿Es este el grafico que desea generar? (y/n)")
         confirmacion = input().upper()
         if confirmacion != "Y":
             if confirmacion != "N":
                 print("Opción no valida")
+    return id
+
+def ShowOptions():
+    print("¿Desea ver los graficos que se pueden construir? (y/n)")
+    responseOfUser = input()
+    if responseOfUser.upper() == "Y":
+        getData = requests.get("https://api.bcra.gob.ar/estadisticas/v1/principalesvariables", verify=False)
+        for data in getData.json()['results']:
+            id = str(data['idVariable'])
+            description = data['descripcion']
+            print('El id: ' + id + ', genera: ' + description)
+        
+
+
+def Do(onlyResultsVariables):
+    ShowOptions()
+    id = GetId(onlyResultsVariables)
     print("Ingrese fecha desde cuando inice el grafico (dd/mm/yyyy):")
     fechaDesde = input().split("/")
+    diaDesde = fechaDesde[0]
+    mesDesde = fechaDesde[1]
+    anioDesde = fechaDesde[2]
     print("Ingrese fecha hasta cuando mostrar los datos (dd/mm/yyyy):")
     fechaHasta = input().split("/")
+    diaHasta = fechaHasta[0]
+    mesHasta = fechaHasta[1]
+    anioHasta = fechaHasta[2]
+    CreateGraph(int(id), diaDesde, mesDesde, anioDesde, diaHasta, mesHasta, anioHasta, onlyResultsVariables)
+
+if __name__ == '__main__':
+    urllib3.disable_warnings()
+    response = requests.get("https://api.bcra.gob.ar/estadisticas/v1/principalesvariables", verify=False)
+
+    responseInJson = response.json()
+    onlyResultsVariables = responseInJson["results"]
+
     try:
-        CreateGraph(int(id), fechaDesde[0],fechaDesde[1], fechaDesde[2], fechaHasta[0], fechaHasta[1], fechaHasta[2])
+        Do(onlyResultsVariables)
     except IndexError:
-        print("Error el formato de la fecha colocada")
-except NameError:
-    print("No hay grafico que se pueda generar con ese id")
-except ValueError:
-    print("No hay grafico que se pueda generar con ese id")
-except:
-    print("Opción no valida")
+            print("Error el formato de la fecha colocada")    
+    except NameError:
+        print("No hay grafico que se pueda generar con ese id")
+    except ValueError:
+        print("No hay grafico que se pueda generar con ese id")
+    
 
